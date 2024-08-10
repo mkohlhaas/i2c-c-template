224a989439cfee97d6a2a2c65c85a3e00c964d05
Hans-J. Schmid <knock@myopendoor.de>
2024-08-10 15:48:16 +0200

    bug fix

--- i2cdriver.c
+++ i2cdriver.c
@@ -418,16 +418,16 @@ i2c_write_register (i2c_handle sd, uint8_t device, uint8_t reg, size_t count, ui
   uint8_t write_buffer[count + 1];
   write_buffer[0] = reg;
   memcpy (write_buffer + 1, buffer, count);
-  i2c_start (sd, device, write_op);
-  {
-    dbgPrintExt ("Couldn't start write operation");
-    return false;
-  }
-  i2c_write_buffer (sd, count + 1, write_buffer);
-  {
-    dbgPrintExt ("Couldn't write buffer");
-    return false;
-  }
+  if (!i2c_start (sd, device, write_op))
+    {
+      dbgPrintExt ("Couldn't start write operation");
+      return false;
+    }
+  if (!i2c_write_buffer (sd, count + 1, write_buffer))
+    {
+      dbgPrintExt ("Couldn't write buffer");
+      return false;
+    }
   i2c_stop (sd);
 
   crc_update (sd, buffer, count);
